3ebbbe8e974c67d9edd26e1dc0e2cf4edd3d50b7
capybara-sea <g1053356495@gmail.com>
2022-12-05 16:23:07 +0800

    set base-url

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Evacuation-Guidelines/',
   plugins: [
     commonjs(),
     vue(),
