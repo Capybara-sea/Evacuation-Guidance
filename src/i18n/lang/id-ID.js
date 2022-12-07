9aa4203cd3466a3f17b29691dc52c0ac0a28bd3c
garcia <garciatyoutyou@gmail.com>
2022-12-07 22:26:43 +0900

    update indonesia

---
+++ src/i18n/lang/id-ID.js
@@ -0,0 +1,101 @@
+export default {
+  code: 'id-ID',
+  name: 'Bahasa Indonesia',
+  home: {
+    // 外国人向け避難誘導
+    question: 'Panduan Evakuasi bagi orang asing',
+    // 災害時多言語情報
+    info: 'Informasi berkaitan bencana berbagai bahasa',
+  },
+  content: {
+    // 日本語はわかりますか?
+    1: 'Apakah Anda memahami bahasa Jepang?',
+    // 何語がわかりますか
+    2: 'Anda memahami bahasa apa saja?',
+    // 翻訳アプリ(参考)
+    3: 'Anda dapat merujuk ke perangkat lunak terjemahan berikut.',
+    // 何か困っていることはありますか？
+    4: 'Apakah ada hal-hal yang menjadi permasalahan?',
+    // 欲しいものはありますか？
+    5: 'Apakah ada barang yang dibutuhkan?',
+  },
+  button: {
+    // はい
+    yes: 'ya',
+    // いいえ
+    no: 'Tidak',
+    // 確認
+    comfirm: 'konfirmasi',
+  },
+  list: {
+    lang: {
+      en: '英語',
+      zh: '中国語(簡体字)',
+      ko: '韓国語',
+      vi: 'ベトナム語',
+      ne: 'ネパール語',
+      tl: 'タガログ語',
+      id: 'インドネシア語',
+      zhTW: '中国語（繁体字）',
+      th: 'タイ語',
+      mm: 'ミャンマー語',
+      es: 'スペイン語',
+      fr: 'フランス語',
+      pt: 'ポルトガル語',
+      ru: 'ロシア語',
+      el: 'その他',
+    },
+    help: {
+      // 食べられないものがあります
+      1: 'Ada jenis makanan yang tidak dapat dimakan',
+      // 長い間持っている病気があります
+      2: 'Ada penyakit kronis',
+      // 妊娠しています
+      3: 'Saya hamil',
+      // 障害があります
+      4: 'Saya penyandang cacat',
+      // 体の調子が悪いです
+      5: 'Saya tidak enak badan',
+      // お祈りをしたいです
+      6: 'Saya mau sembahyang',
+      // 授乳をしたいです
+      7: 'Saya ingin menyusui anak',
+      // 短い間個室を使いたいです
+      8: 'Saya ingin memakai ruang privat sementara',
+      // ペットを連'れてきたいです
+      9: 'Saya ingin membawa peliharaan saya',
+      //  Wi-Fiを使いたいです
+      10: 'Saya ingin memakai Wi-fi',
+      //  携帯電話を充電したいです
+      11: 'Saya mau mengecharge HP saya',
+      //  家族に連絡したいです
+      12: 'Saya mau menghubungi keluarga saya',
+      //  大使館に連絡したいです
+      13: 'Saya mau menghubungi Kedutaan Besar',
+      //  すぐに帰国したいです
+      14: 'Saya mau pulang ke negara saya secepatnya',
+      //  移動方法を知りたいです
+      15: 'Saya ingin mengetahui cara transfer/perjalanan',
+      //  外貨両替したいです
+      16: 'Saya ingin menukar uang',
+    },
+    need: {
+      // 水
+      1: 'Air',
+      // 毛布
+      2: 'Selimut',
+      // おむつ
+      3: 'Popok',
+      // 生理用品
+      4: 'Pembalut untuk menstruasi',
+      // 食べもの
+      5: 'Makanan',
+      // ハラル食品
+      6: 'Makanan halal',
+      // くすり
+      7: 'Obat',
+      // ミルク
+      8: 'Susu',
+    },
+  },
+}
