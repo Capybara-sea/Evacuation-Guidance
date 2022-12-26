3da574e6f0f93f102375322e1e5b9b4e2ac6c3bc
capybara-sea <g1053356495@gmail.com>
2022-12-27 02:43:06 +0800

    🐞fix(router): add scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -33,9 +33,18 @@ const routes = [
   },
 ]
 
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition
+  } else {
+    return { top: 0 }
+  }
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior,
 })
 
 router.beforeEach((to, from) => {
