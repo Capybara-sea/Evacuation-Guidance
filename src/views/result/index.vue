e389c161771a745eb867271919232fcfb067c6ec
capybara-sea <g1053356495@gmail.com>
2022-12-27 02:44:43 +0800

    🦄refactor(result page): Change the interaction of export images

--- src/views/result/index.vue
+++ src/views/result/index.vue
@@ -1,42 +1,54 @@
 <template>
-  <div v-loading="loading">
-    <!--  -->
-    <template v-if="!imgUrl">
-      <el-descriptions :column="1" border ref="cell">
-        <i18n-box class="cell-left" t="name" size="small" />
+  <div>
+    <el-descriptions :column="1" border ref="cell">
+      <i18n-box class="cell-left" t="name" size="small" />
 
-        <template v-for="item in script">
-          <el-descriptions-item v-if="result[item.id]">
-            <!-- title -->
-            <template #label>
-              <i18n-box class="cell-left" :t="item.content" size="small" />
+      <template v-for="item in script">
+        <el-descriptions-item v-if="result[item.id]">
+          <!-- title -->
+          <template #label>
+            <i18n-box class="cell-left" :t="item.content" size="small" />
+          </template>
+
+          <!-- result -->
+          <div class="cell-right">
+            <template v-if="item.type == 'check'">
+              <i18n-box :t="result[item.id]" size="small" />
             </template>
-
-            <!-- result -->
-            <div class="cell-right">
-              <template v-if="item.type == 'check'">
-                <i18n-box :t="result[item.id]" size="small" />
-              </template>
-              <template v-else-if="item.type == 'grid'">
-                <i18n-box class="grid" v-for="text in result[item.id]" :t="text" size="small" />
-              </template>
-            </div>
-          </el-descriptions-item>
-        </template>
-      </el-descriptions>
-    </template>
-
-    <!--  -->
-    <template v-else>
-      <img class="result-img" :src="imgUrl" alt="" />
-    </template>
+            <template v-else-if="item.type == 'grid'">
+              <i18n-box
+                class="grid"
+                v-for="text in result[item.id]"
+                :t="text"
+                size="small"
+              />
+            </template>
+          </div>
+        </el-descriptions-item>
+      </template>
+    </el-descriptions>
   </div>
+
+  <div class="button-box">
+    <my-button plain @click="getImage"> 生成图片 </my-button>
+  </div>
+  <el-dialog v-model="showImage" fullscreen>
+    <div class="image-box">
+      <el-alert
+        title="长按保存图片"
+        type="success"
+        :closable="false"
+        center
+        show-icon
+      />
+      <img :src="imgUrl" />
+    </div>
+  </el-dialog>
 </template>
 
 <script setup>
 import script from '@/script'
 import { useStore } from '@/store'
-import { onMounted } from 'vue'
 import { ref, computed } from '@vue/reactivity'
 import dom2image from '@/utils/dom2image'
 import 'element-plus/es/components/message/style/css'
@@ -47,7 +59,13 @@ const devResult = {
   1: 'button.yes',
   2: ['list.lang.en-US', 'list.lang.vi-VN', 'list.lang.id-ID'],
   4: 'button.yes',
-  5: ['list.help.1', 'list.help.3', 'list.help.5', 'list.help.8', 'list.help.7'],
+  5: [
+    'list.help.1',
+    'list.help.3',
+    'list.help.5',
+    'list.help.8',
+    'list.help.7',
+  ],
   6: 'button.yes',
   7: ['list.need.3', 'list.need.5'],
 }
@@ -58,19 +76,18 @@ const result = computed(() => {
 
 const cell = ref()
 const imgUrl = ref()
-const loading = ref(true)
+const showImage = ref()
 
-onMounted(async () => {
+async function getImage() {
   try {
     const url = await dom2image(cell.value.$el, 0)
     imgUrl.value = url
     ElMessage.success('Exporting images successfully!')
+    showImage.value = true
   } catch (error) {
     ElMessage.error('Oops, failed to export the image.')
-  } finally {
-    loading.value = false
   }
-})
+}
 </script>
 
 <style lang="scss" scoped>
@@ -96,7 +113,19 @@ onMounted(async () => {
     }
   }
 }
-.result-img {
-  width: 100vw;
+.button-box {
+  width: 100%;
+  padding: 8px;
+}
+.image-box {
+  div {
+    text-align: center;
+  }
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: contain;
+    padding: 10px;
+  }
 }
 </style>
