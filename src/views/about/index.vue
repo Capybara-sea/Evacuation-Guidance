957ca5e578107c12dec8460baf67b7b76e5b79ad
capybara-sea <g1053356495@gmail.com>
2023-02-24 21:56:31 +0800

    ✨feat(pages): update about

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="github">
-    <el-link
-      href="https://github.com/Capybara-sea/Evacuation-Guidance"
-      type="primary"
-    >
-      <svg-icon name="github" />Github
-    </el-link>
+  <div class="about-container">
+    <div>Capybara-sea & queen0309</div>
+    <div class="github">
+      <el-link href="https://github.com/Capybara-sea/Evacuation-Guidance" type="primary">
+        <svg-icon name="github" />Github
+      </el-link>
+    </div>
   </div>
-  <div>Capybara-sea & queen0309</div>
 </template>
 
 <style lang="scss" scoped>
-.github {
-  margin: 12px;
+.about-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 80vh;
+  gap: 12px;
 }
 </style>
